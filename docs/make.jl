322a097a0dccf2a8a249156c4c53e8fd23da108e
Gael Forget <gforget@mit.edu>
2019-04-24 14:01:59 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,12 @@ makedocs(
     sitename = "CbiomesProcessing",
     format = :html,
     modules = [CbiomesProcessing]
-    pages = Any[
-    "Home" => "index.md",
-    "Codes" => "sepdev1.md"
-    ]
 )
+
+#    pages = Any[
+#    "Home" => "index.md",
+#    "Codes" => "sepdev1.md"
+#    ]
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
