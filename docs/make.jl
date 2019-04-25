19de1f80ccb3f1b720928c05546142336bbbb98a
Gael Forget <gforget@mit.edu>
2019-04-24 20:39:28 -0400

    link to travis to generate documentation page (#1)
    * Create .travis.yml
    * Update make.jl
    * Update .gitignore
    * Update .travis.yml
    * Update README.md

--- docs/make.jl
+++ docs/make.jl
@@ -5,15 +5,16 @@ makedocs(
     sitename = "CbiomesProcessing",
     format = :html,
     modules = [CbiomesProcessing]
-    pages = Any[
-    "Home" => "index.md",
-    "Codes" => "sepdev1.md"
-    ]
 )
+
+#    pages = Any[
+#    "Home" => "index.md",
+#    "Codes" => "sepdev1.md"
+#    ]
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/gaelforget/CBIOMES-Processing.jl.git",
+)
